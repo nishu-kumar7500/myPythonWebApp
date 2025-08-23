f72e1359e2bfd966b0773a6246ccfe7bf0e35762
DevOps7500 <132784995+nishu-kumar7500@users.noreply.github.com>
2025-08-23 10:22:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.get("/")
-def root():
-    return {"message": "Hello from Flask on Azure!"}, 200
+def home():
+    # This will render templates/index.html
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    # Local dev only; Azure will use gunicorn
+    # Local dev
     app.run(host="0.0.0.0", port=5000)
