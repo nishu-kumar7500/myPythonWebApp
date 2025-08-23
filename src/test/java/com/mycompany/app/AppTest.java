249cd966b81b4fbeb6dd0456539a4d2eee80c95f
DevOps7500 <132784995+nishu-kumar7500@users.noreply.github.com>
2025-08-23 09:31:00 +0530

    Create AppTest.java

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true); // Simple test
+    }
+}
